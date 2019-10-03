71b48606aab83cea18d62ce655c3aa0c6ef6887a
Dmitry <irvis@imega.ru>
2019-10-03 10:33:22 +0300

    wrapper for grid and cell

--- src/GridLayoutEditor/Editor.js
+++ src/GridLayoutEditor/Editor.js
@@ -1,6 +1,30 @@
 import React from "react";
+import styled from "react-emotion";
 
 import Matrix from "./Matrix";
+
+const styleGrid = (props, template) => {
+    return styled(props.grid)`
+        grid-template-areas: ${template};
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(3, 1fr);
+    `;
+};
+
+const styleCell = (props, cell) => {
+    const CellStyle = styled(props.cell)`
+        grid-area: ${cell.name};
+    `;
+    return <CellStyle key={cell.name} {...props} />;
+};
+
+const MapIterator = (map, cb) => {
+    const result = [];
+    for (let [key, value] of map) {
+        result.push(cb(value, key));
+    }
+    return result;
+};
 
 class Editor extends React.Component {
     matrix = Matrix({
@@ -8,7 +32,13 @@ class Editor extends React.Component {
     });
 
     render = () => {
-        return <div>Editor</div>;
+        const g = this.matrix.toGrid();
+        const Grid = styleGrid(this.props.theme, g.template);
+        return (
+            <Grid>
+                {MapIterator(g.cells, i => styleCell(this.props.theme, i))}
+            </Grid>
+        );
     };
 }
 
