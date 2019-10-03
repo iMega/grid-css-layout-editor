9d367de737bad7140cdead9bedeb4d5f201f774e
Dmitry <irvis@imega.ru>
2019-10-03 10:32:31 +0300

    add basic theme for grid and cell style

--- src/themes/BasicTheme.js
+++ src/themes/BasicTheme.js
@@ -7,6 +7,16 @@ const BasicTheme = () => {
             background: #edf1f5;
             border-top: 2px dashed #777;
             border-left: 2px dashed #777;
+        `,
+        cell: styled.div`
+            min-width: 120px;
+            min-height: 120px;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+            display: flex;
+            border-bottom: 2px dashed #777;
+            border-right: 2px dashed #777;
         `
     };
 };
