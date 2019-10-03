7bc5baa05d7fff66a9d158520f91352b2c79d698
Dmitry <irvis@imega.ru>
2019-10-03 10:34:17 +0300

    Editor with Theme

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 
+import { ThemeProvider, withTheme } from "./ThemeProvider";
+import { themes } from "./themes";
 import Editor from "./GridLayoutEditor";
 
-const Main = () => <Editor />;
+const Component = withTheme(Editor);
+
+const Main = props => (
+    <ThemeProvider theme={props.theme} size={props.size} themes={themes}>
+        <Component {...props} />
+    </ThemeProvider>
+);
+
+Main.defaultProps = {
+    theme: "light",
+    size: "normal"
+};
+
+Main.propTypes = {
+    theme: PropTypes.string,
+    size: PropTypes.string
+};
 
 export default Main;
